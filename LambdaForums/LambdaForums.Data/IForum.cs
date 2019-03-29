9f93fce36b8acdcfb1728b3d631429554f1d0c5f
safayat-zisan <zisan94268@gmail.com>
2019-03-30 03:46:32 +0600

    no message

---
+++ LambdaForums/LambdaForums.Data/IForum.cs
@@ -0,0 +1,20 @@
+﻿using LambdaForums.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaForums.Data
+{
+    public interface IForum
+    {
+        Forum GetById(int id);
+        IEnumerable<Forum> GetAll();
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+        Task Create(Forum forum);
+        Task Delete(int forumId);
+        Task UpdateForumTitle(int forumId, string newTitle);
+        Task updateForumDescription(int forumId, string newDescription);
+    }
+}
